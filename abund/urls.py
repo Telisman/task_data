afa34b2008e5a6f3339f940911ff0023b7674c3d
Davor Telisman <davortelisman@gmail.com>
2023-07-14 14:58:04 +0200

    Add in are view.py are main url path for are task

--- abund/urls.py
+++ abund/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import home,your_api_endpoint
 urlpatterns = [
-    path('home/', home)
+    path('', home),
+    path('endpoint/', your_api_endpoint, name='your_api_endpoint'),
+
 ]
